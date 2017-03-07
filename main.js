e3e5b9e156e819374cbb1cc65e36c9fc484ab766
Athena Perez <athenaperez@me.com>
2017-03-07 10:56:17 -0600

    learning to dig down and display specific information within database

--- main.js
+++ main.js
@@ -9,8 +9,44 @@ const dropEmployees = () => {
 };
 //dropEmployees();
 
-db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary NUMBER(6,2))");
 
-//db.run(`INSERT INTO employees VALUES (1, "Ashley", "Irwin", 5)`)
+// Creates the employee table if it does not exist
+// Will not execute if the table exists
+db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT, department TEXT)");
 
 
+//populates employee table with json data
+const populateEmployees = () => {
+
+	const { list } = require('./employees.json');
+
+	list.forEach( each => {
+		db.run(`INSERT INTO employees VALUES (
+			${each.id},
+			"${each.firstName}",
+			"${each.lastName}",
+			${each.salary},
+			"${each.department}"
+			)`);
+	})
+};
+//populateEmployees();
+
+
+//not the best option - only selects the first row
+// db.get(`SELECT * FROM employees`, (err, row) => {
+// 	console.log(row)
+// });
+
+
+//retrieves all info
+db.all(`SELECT * FROM employees`, (err, allRows) => {
+	console.log(allRows);
+	allRows.forEach(( {id, first, last, department, salary} ) => {
+		console.log(`
+		${id} ${first} ${last}
+		from ${department} Department.
+		Salary: ${salary}
+		`);
+	});
+});
